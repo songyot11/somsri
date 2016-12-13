b0294ec2e9b74c8be8b218d43576dafa3ad6e551
Kraiwut Arayangkura <kraiwuthi.a@gmail.com>
2016-12-13 11:13:21 +0700

    setup devise

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'jquery-rails'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
 
+gem 'devise'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  unauthenticated :user do
+    devise_scope :user do
+      get "/" => "devise/sessions#new"
+    end
+  end
+
+  get "/" => "home#index"
 end
