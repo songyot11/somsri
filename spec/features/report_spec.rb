74d14c9f494f99985056c8c3fa490b4d1dc76b88
Pondpaun7z <pppangpondpp7@gmail.com>
2016-12-15 16:41:50 +0700

    Added rpsec on report without swich month

--- spec/features/report_spec.rb
+++ spec/features/report_spec.rb
@@ -1,12 +1,15 @@
-describe 'Payroll Report', js: true do
-  let(:user)    {user = User.make!}
-
-  
+describe 'Payroll Report', js: true do  
   it 'should see header table' do
     visit "/#/report"
 
-    eventually { expect(page).to have_content 'รหัส + ชื่อ + เลขบัญชี เงินเดือน เงินหัก เงินเพิ่ม เงินเดือนสุทธิ' }
+    eventually { expect(page).to have_content 'รหัส ชื่อ เลขบัญชี เงินเดือน เงินหัก เงินเพิ่ม เงินเดือนสุทธิ' }
+    eventually { expect(page).to have_content 'รวมทั้งหมด' }
   end
 
+  it 'should see month list' do
+    visit "/#/report"
 
+    find('#month-list').click
+    eventually { expect(page).to have_content 'มกราคม 2016 กุมพาพันธ์ 2016' }
+  end
 end
