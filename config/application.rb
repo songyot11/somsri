4125ea85b71a875a94203904f6d24ba84772d4fb
Jirapong Nanta <jirapong.nanta@gmail.com>
2017-04-04 17:08:26 +0700

    default timezone to Bangkok

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module SomsriPayroll
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Bangkok'
     config.i18n.default_locale = :th
     config.i18n.available_locales = [:th, :en]
   end
