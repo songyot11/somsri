9f2d02ca1326fb9c492a9a28ee94c33378399ebe
akiyama-eve <tawan@bananacoding.com>
2017-05-25 16:05:47 +0700

    add payroll slip header (#308)
    * add payroll slip header
    
    * fix spec

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522104634) do
+ActiveRecord::Schema.define(version: 20170525065816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -286,6 +286,7 @@ ActiveRecord::Schema.define(version: 20170522104634) do
     t.string   "logo_content_type"
     t.integer  "logo_file_size"
     t.datetime "logo_updated_at"
+    t.text     "payroll_slip_header"
   end
 
   create_table "student_lists", force: :cascade do |t|
