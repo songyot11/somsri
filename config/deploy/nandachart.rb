89ab1104f8b2dc3681073ae6416c34ea3fe1660d
ssmanit <manit@bananacoding.com>
2017-08-25 11:31:02 +0700

    change nandachart deploy brance to master

--- config/deploy/nandachart.rb
+++ config/deploy/nandachart.rb
@@ -5,7 +5,7 @@
 
 set :application, 'somsri-nandachart'
 server '150.95.25.211', user: 'deploy', roles: %w{app db web}
-set :branch, ENV['BRANCH'] || 'nandachart'
+set :branch, ENV['BRANCH'] || 'master'
 set :deploy_to, '/srv/www/apps/somsri-nandachart'
 
 
