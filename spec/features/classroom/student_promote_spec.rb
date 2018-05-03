484110dd43bac5379d6466fc04c3bb086ac2759f
Kraiwut Arayangkura <kraiwuthi.a@gmail.com>
2018-05-03 14:54:50 +0700

    fix timing issue

--- spec/features/classroom/student_promote_spec.rb
+++ spec/features/classroom/student_promote_spec.rb
@@ -75,7 +75,7 @@ describe 'Student Promote', js: true do
 
     it 'should go to next classroom management' do
       click_button("เลื่อนชั้นเรียน")
-      sleep(1.5)
+      sleep(2)
       eventually { expect(page).to have_content("ระดับชั้นเรียน เดิม") }
       eventually { expect(page).to have_content("ระดับชั้นเรียนใหม่") }
     end
