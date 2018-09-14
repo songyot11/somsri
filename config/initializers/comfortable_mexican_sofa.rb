878f800cabbba8ab32a9d7e9efe02345b5045c99
akiyama-eve <tawan@bananacoding.com>
2018-09-14 10:14:05 +0700

    fix cms upload img to s3

--- config/initializers/comfortable_mexican_sofa.rb
+++ config/initializers/comfortable_mexican_sofa.rb
@@ -35,7 +35,7 @@ ComfortableMexicanSofa.configure do |config|
   # http://rdoc.info/gems/paperclip/2.3.8/Paperclip/Storage/S3, and for
   # filesystem see: http://rdoc.info/gems/paperclip/2.3.8/Paperclip/Storage/Filesystem
   # If you are using S3 and HTTPS, pass :s3_protocol => '' to have URLs that use the protocol of the page
-  config.upload_file_options = {:url => '/system/:class/:id/:attachment/:style/:filename', styles: { menu_item: '175x115>', thumb: '100x100<', medium: '300x300<'} }
+  config.upload_file_options = {:url => '/system/:class/:id/:attachment/:style/:filename', styles: { menu_item: '175x115>', thumb: '100x100<', medium: '300x300<'}, :s3_permissions => 'public-read' }
 
   # Sofa allows you to setup entire site from files. Database is updated with each
   # request (if necessary). Please note that database entries are destroyed if there's
