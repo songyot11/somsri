48e6f69f318f6d89b2d68550583dbe3b92f31657
Teerapong Chantakard <azygous13@gmail.com>
2018-11-14 01:35:11 +0700

    [Added] half day leave for vacation and deduce_days

--- app/controllers/vacations_controller.rb
+++ app/controllers/vacations_controller.rb
@@ -7,19 +7,37 @@ class VacationsController < ApplicationController
     end_date = Date.new(year, 12, 31).end_of_day
     @vacations = @vacations.where('created_at BETWEEN ? AND ?', start_date, end_date)
 
-    sick_leave = @vacations.where(vacation_type: VacationType.where(name: 'ลาป่วย').first.id).count
-    vacation_leave = @vacations.where(vacation_type: VacationType.where(name: 'ลากิจ').first.id).count
-    switch_date = @vacations.where(vacation_type: VacationType.where(name: 'สลับวันทำงาน').first.id).count
-    work_at_home = @vacations.where(vacation_type: VacationType.where(name: 'ทำงานที่บ้าน').first.id).count
-    remaining_day = current_user.leave_allowance - vacation_leave
+    type_sick_leave = VacationType.where(name: 'ลาป่วย').first
+    type_full_day_leave = VacationType.where(name: 'ลากิจ').first
+    type_half_day_morning_leave = VacationType.where(name: 'ลากิจครึ่งวันเช้า').first
+    type_half_day_afternoon_leave = VacationType.where(name: 'ลากิจครึ่งวันบ่าย').first
+    type_swtich_date = VacationType.where(name: 'สลับวันทำงาน').first
+    type_work_at_home = VacationType.where(name: 'ทำงานที่บ้าน').first
+
+    sick_leave_count = @vacations.where(vacation_type: type_sick_leave.id).count
+    full_day_leave_count = @vacations.where(vacation_type: type_full_day_leave.id).count
+    half_day_morning_leave_count = @vacations.where(vacation_type: type_half_day_morning_leave.id).count
+    half_day_afternoon_leave_count = @vacations.where(vacation_type: type_half_day_afternoon_leave.id).count
+    vacation_leave = full_day_leave_count + half_day_morning_leave_count + half_day_afternoon_leave_count
+    switch_date_count = @vacations.where(vacation_type: type_swtich_date.id).count
+    work_at_home_count = @vacations.where(vacation_type: type_work_at_home.id).count
+
+    deduce_days = 0
+    deduce_days += sick_leave_count * type_sick_leave.deduce_days
+    deduce_days += full_day_leave_count * type_full_day_leave.deduce_days
+    deduce_days += half_day_morning_leave_count * type_half_day_morning_leave.deduce_days
+    deduce_days += half_day_afternoon_leave_count * type_half_day_afternoon_leave.deduce_days
+    deduce_days += switch_date_count * type_swtich_date.deduce_days
+    deduce_days += work_at_home_count * type_work_at_home.deduce_days
+    remaining_day = current_user.leave_allowance - deduce_days
 
     render json: {
       leave_allowance: current_user.leave_allowance,
       remaining_day: remaining_day,
-      sick_leave: sick_leave,
+      sick_leave: sick_leave_count,
       vacation_leave: vacation_leave,
-      switch_date: switch_date,
-      work_at_home: work_at_home,
+      switch_date: switch_date_count,
+      work_at_home: work_at_home_count,
       vacations: @vacations
     }, status: :ok
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181112051839) do
+ActiveRecord::Schema.define(version: 20181113175656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -638,8 +638,9 @@ ActiveRecord::Schema.define(version: 20181112051839) do
 
   create_table "vacation_types", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.float    "deduce_days"
   end
 
   create_table "vacations", force: :cascade do |t|
