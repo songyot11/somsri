8ef8bf3d6ee92945f8841602c392682d683b78d2
Teerapong Chantakard <azygous13@gmail.com>
2018-11-29 13:14:43 +0700

    [Fixed] employee cannot logout (#618)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,6 +30,16 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :user
+      new_user_session_path
+    elsif resource_or_scope == :employee
+      new_employee_session_path
+    else
+      root_path
+    end
+  end
+
   def isDate(date_string)
     begin
        Date.parse(date_string)
