f48132aac920c72acab9890ba1aefc10d628f03b
Tong <theerapong@bananacoding.com>
2018-12-24 10:42:31 +0700

    create filter inventory

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -5,13 +5,18 @@ class InventoriesController < ApplicationController
 	def index
 		page = params[:page]
 		search = params[:search_keyword]
-		# inventories = get_inventories(params[:search_keyword],params[:page])
+		start_date_purchase = DateTime.parse(params[:start_date_purchase]).beginning_of_day if isDate(params[:start_date_purchase])
+    end_date_purchase = DateTime.parse(params[:end_date_purchase]).end_of_day if isDate(params[:end_date_purchase])
+		
+		# inventories = get_inventories(params[:search_keyword],start_date_purchase,end_date_purchase,params[:page])
 		# if params[:page] && inventories.total_pages < inventories.current_page
-		# 	inventories = get_inventories(params[:search_keyword])
+		# 	inventories = get_inventories(params[:search_keyword],start_date_purchase,end_date_purchase)
 		# end
 
 		inventories = Inventory.all
     inventories = inventories.search(search) if search.present?
+    data_field = Inventory.arel_table[:created_at]
+    inventories = qry_date_range(inventories, data_field, start_date_purchase, end_date_purchase)
     inventories = inventories.paginate(page: page, per_page: 10)
 
 		result = {}
@@ -78,8 +83,12 @@ class InventoriesController < ApplicationController
 		params.require(:inventory).permit(:item_name, :serial_number, :model, :description, :price, :date_purchase, :category, :category_barcode, :date_add, :end_warranty ,:employee_id)
 	end
 
-	def get_inventories(search_keyword,page)
+	def get_inventories(search_keyword,start_date_purchase,end_date_purchase,page)
 		inventories = Inventory.all.search(search_keyword) #if search_keyword.present?
+		data_field = Inventory.arel_table[:created_at]
+    inventories = qry_date_range(inventories, data_field, start_date_purchase, end_date_purchase)
+
+    # qry_invoices = qry_invoices.order("#{sort} #{order}")
 		inventories = inventories.paginate(page: page, per_page: 10)
 		return inventories.to_a
 	end
