cd355be940406bb1a1f7259377036b3d5ecefa91
Thongchat Teparuk <thongchat.tr@gmail.com>
2019-06-24 14:14:39 +0700

    can update

--- app/controllers/scouts_controller.rb
+++ app/controllers/scouts_controller.rb
@@ -1,21 +1,32 @@
 class ScoutsController < ApplicationController
-  def index
-    @candidate = Candidate.find_by(id: 1)
-    render json: @candidate, status: :ok
+  
+  def edit
+    render json: getInfo(params[:id]), status: :ok
   end
   
   def update_candidate
     @candidate = Candidate.find_by(id: params[:id])
     @candidate.update(params_candidate)
+    @programming_skill = ProgrammingSkill.find_by(id: params[:id])
+    @programming_skill.update(params_programming_skill)
   end
-
 
 
   private
 
+  def getInfo(id)
+    {
+      candidate: Candidate.find_by(id: id),
+      programming_skill: ProgrammingSkill.find_by(id: id) 
+    }
+  end
 
   def params_candidate
     params.require(:candidate).permit(:full_name, :nick_name, :email)
+  end
+
+  def params_programming_skill
+    params.require(:programming_skill).permit(:skill_name, :skill_point)
   end
 
   
