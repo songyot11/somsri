eaf2463c2879161c458f94e5692a6006da6b09fb
Songyot Jaichai <songyot_jaichai@hotmail.com>
2019-06-24 21:11:06 +0700

    search and sort

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,7 +1,8 @@
 class CandidatesController < ApplicationController
 
   def index
-    @candidate = Candidate.all
+    @candidate = Candidate.all.order(created_at: 'desc')
+    filter_parents
     total = @candidate.size
     @candidate = @candidate.offset(params[:offset]).limit(params[:limit])
     
@@ -15,5 +16,33 @@ class CandidatesController < ApplicationController
     @candidate = Candidate.find_by(id: params[:id])
     @candidate.destroy
   end
+
+  def filter_parents
+    if params[:search].present?
+      search = "%#{params[:search]}%"
+      @candidate = @candidate.where('full_name LIKE :search OR
+                                  nick_name LIKE :search OR
+                                  email LIKE :search
+                                  ',
+                                  search: search)
+    end
+      # sort and order
+      if params[:sort].present?
+        case params[:sort]
+        when 'link_full_name'
+          @candidate = Candidate.order(full_name: params[:order])
+        when 'school_year'
+          @candidate = Candidate.order(school_year: params[:order])
+        when 'current_ability'
+          @candidate = Candidate.order(current_ability: params[:order])
+        when 'learn_ability'
+          @candidate = Candidate.order(learn_ability: params[:order])
+        when 'created_at'
+          @candidate = Candidate.order(created_at: params[:order])
+        end
+      end
+  end  
+
+  
 
 end
