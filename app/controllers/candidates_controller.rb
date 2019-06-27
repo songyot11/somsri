aef13739bda83447ed977f113aa979e3a60d9d00
Songyot Jaichai <songyot_jaichai@hotmail.com>
2019-06-27 15:08:05 +0700

    [Fixed] code

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -10,6 +10,11 @@ class CandidatesController < ApplicationController
       total: total
     }, status: :ok
   end
+
+  def create
+    @candidate = Candidate.new(candidate_params)
+    @candidate.save
+  end
   
   def destroy
     @candidate = Candidate.find_by(id: params[:id])
@@ -52,4 +57,20 @@ class CandidatesController < ApplicationController
 
   
 
+  private
+
+  def upload_photo_params
+    params.require(:candidate).permit(:file)
+  end
+
+  def candidate_params
+    params.permit(:full_name, :nick_name, :email,:phone, 
+      :from, :school_year, :note, :current_ability,
+      :learn_ability, :attention, :interest,
+      programming_skills_attributes: [:id, :skill_name, :skill_point, :_destroy],
+      soft_skills_attributes: [:id, :skill_name, :skill_point, :_destroy],
+      design_skills_attributes: [:id, :skill_name, :skill_point, :_destroy],
+      candidate_files_attributes: [:id, :files, :_destroy]
+    )
+  end
 end
